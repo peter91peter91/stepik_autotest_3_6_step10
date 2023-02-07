b9151da2460c8187a191328531b1bc6090b90f59
Moiseev PA <p.moiseev@big3.ru>
2023-02-07 17:34:22 +0300

    stepik_autotest_3_6_step10   try_4

--- stepik_3_6_10/conftest.py
+++ stepik_3_6_10/conftest.py
@@ -1,37 +1,32 @@
-"""старая версия test_items.py
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
-"""
+#Распознаёт фаерфокс, хром.(если указать в парам. запуска   --browser_name=....  )
+#Изменяет язык на необходимый(если указать в парам. запуска   --language=....  )
+#Пожалуйста,проверьте, что у вас нет своего conftest.py в директории выше.
 
 import pytest
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
-
+    parser.addoption('--language', action='store', default='es',
+                     help="Choose language")
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    browser = None
+    user_language = request.config.getoption("language")
     if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        print("\nstart browser CHROME for test...")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        print("\nstart browser FIREFOX for test...")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        print("Browser {} still is not implemented".format(browser_name))
     yield browser
-    print("\nquit browser..")
+    print("\nquit browser...")
     browser.quit()
